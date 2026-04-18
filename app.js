d91400b330d00d52e533004b7db4d8d1494d38b1
oneoneon522 <jj0985788718@gmail.com>
2026-04-18 16:01:59 +0800

    add views

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
 // app.js
 import express from "express";
-
+import mysqlConnectionPool from "./lib/mysql.js";
 const app = express();
 
-app.get("/ping", (req, res) => {
-  return res.send("<h1>Pong!</h1>");
+// first middleware
+app.use(express.static('.'))
+
+// second middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root:'.'});  // 對應 views/index.ejs
+});
+
+app.post("/signup", async (req, res) => {
+  const name = req.body["name"];
+  const email = req.body["email"];
+  const password = req.body["password"];
+
+  await mysqlConnectionPool.query(
+    "INSERT INTO User (Name, Email, Password) VALUES (?, ?, ?)",
+    [name, email, password]
+  );
+  return res.status(201).json({ success: true });
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile('login.html', {root:'.'});
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body["email"];
+  const account = req.body["account"];
+
+  const result = await mysqlConnectionPool.query(
+    "SELECT UserId FROM User WHERE Email = ? AND Password = ?",
+    [email, password]
+  );
+  const rows = result[0];
+
+  if (rows.length === 0) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+  return res.status(200).json({ success: true });
 });
 
 app.listen(3000, () => {
   console.log("Server starts at port 3000");
 });
+
