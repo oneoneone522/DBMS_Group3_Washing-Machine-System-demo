4e6c8260b7b26f6be55270b99f0c7b96a3d75552
oneoneon522 <jj0985788718@gmail.com>
2026-04-20 17:46:40 +0800

    floor machine status check

--- app.js
+++ app.js
@@ -21,9 +21,13 @@ app.use(session({
   cookie: { secure: false }   // 本地開發用 false，上線再改 true
 }));
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 //index
 app.get('/', (req, res) => {
-  res.sendFile('index.html', {root:'./views'});
+  // res.render('index.html', {root:'./views'});
+  res.render('index',{ title: '使用狀態' });
 });
 
 app.get('/api/check-login', (req, res) => {
@@ -48,14 +52,44 @@ app.get('/api/usage_record', async (req, res) => {
   return res.status(200).json(rows);
 });
 
-//machines
-app.get('/machines', (req, res) => {
-  res.sendFile('machines.html', {root:'./views'});
+//machine
+app.get('/machine', (req, res) => {
+  res.render('machine',  {title: '機台使用狀態一覽' });
 });
 
+app.get('/api/machine/my-dorm', async (req, res) => {
+  if (!req.session.user_id) {
+    return res.status(401).json({ message: '請先登入' });
+  }
+  const [userRows] = await mysqlConnectionPool.query(`SELECT Dorm FROM User WHERE User_ID = ?`, [req.session.user_id]);
+  const userDorm = userRows[0].Dorm;
+  const [rows] = await mysqlConnectionPool.query(`
+    SELECT 
+      m.Machine_Number, 
+      m.Machine_Status, 
+      m.Laundry_Room,
+      m.Floor, 
+      m.Dorm,
+      ur.Usage_Status
+    FROM Machine m
+    LEFT JOIN usage_record ur
+    ON m.Machine_ID = ur.Machine_ID
+    WHERE m.Dorm = ?;`,[userDorm]
+  );
+  return res.status(200).json(rows);
+});
+//use_confirm
+app.get('/use_conform', (req, res) =>{
+  res.render('use_confirm', {title: '確認使用機台'});
+});
+
+app.get('/api/machine', async(req, res) => {
+  
+});
 //signup
 app.get('/signup',(req,res) => {
-  res.sendFile('signup.html', {root:'./views'});
+  // res.render('signup.html', {root:'./views'});
+  res.render('signup', {title: '註冊'});
 });
 
 app.get('/api/dorm', async (req, res) => {
@@ -78,7 +112,8 @@ app.post("/signup", async (req, res) => {
 
 //login
 app.get('/login', (req, res) => {
-  res.sendFile('login.html', {root:'./views'});
+  // res.render('login.ejs', {root:'./views'});
+  res.render('login', {title: '登入'});
 });
 
 app.post("/login", async (req, res) => {
